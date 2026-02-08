9d8eae5c0afcc0d838c1c2460f13be7c90e4d446
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-02-09 00:01:12 +0300

    Update Home.tsx

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -14,14 +14,14 @@ export const Home = () => {
       path: '/ferrero',
       description: t('home.ferreroDesc', 'Elegant chocolate creations'),
       image:
-        'https://scontent.famm10-1.fna.fbcdn.net/v/t39.30808-6/488253898_9886438498056854_141848626228315824_n.jpg',
+        'https://scontent.famm10-1.fna.fbcdn.net/v/t39.30808-6/488253898_9886438498056854_141848626228315824_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=JYpSB-fxDokQ7kNvwEol8eR&_nc_oc=Adnro-l0KgwjiO9puIhFLJ8b9YgVFsnGqT-dHBogSgJoez4VVHPUSSf2W5TP7h3IYFKp_NjFMKFeLtf_YrfwuAY-&_nc_zt=23&_nc_ht=scontent.famm10-1.fna&_nc_gid=iMuMKIF7kM2XCOFRDS_S2A&oh=00_Afsl4wqLPleIBTVx_rqCJPc-bepLqH_h7Y_s2-CwqJrzSw&oe=698EC58C',
     },
     {
       name: 'Cheese and Walnut Kullaj 1951',
       path: '/kullaj',
       description: t('home.kullajDesc', 'Traditional artisanal kullaj'),
       image:
-        'https://scontent.famm10-1.fna.fbcdn.net/v/t39.30808-6/490705348_122154620498552292_7629996614415224572_n.jpg',
+        'https://scontent.famm10-1.fna.fbcdn.net/v/t39.30808-6/491559332_122154450566552292_1901582120724717764_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=86c6b0&_nc_ohc=PxNj33cVDdYQ7kNvwF_sgid&_nc_oc=AdkgtuVNSAIu3iEkmhxPgPSexQP-BJl0kD2yxp4les8YHe_NApWAZqZF33Y8B8p7yMHa_3lVOaa7VAbH3oEiD-ST&_nc_zt=23&_nc_ht=scontent.famm10-1.fna&_nc_gid=hFjGEImMYQQP7CC9YesiOg&oh=00_Afu839izbpj262tXYINExM6e2qAVpeX8hax3X7HVylrLcA&oe=698EC1D8',
     },
   ];
 
@@ -29,11 +29,11 @@ export const Home = () => {
     () => [
       {
         name: t('store.categories.cakes'),
-        img: 'https://scontent.famm10-1.fna.fbcdn.net/v/t39.30808-6/352382050_622778522920898_4607408828112141834_n.jpg',
+        img: 'https://instagram.famm13-1.fna.fbcdn.net/v/t51.2885-19/82444459_215139809630697_790629369532907520_n.jpg?efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4xMDgwLmMyIn0&_nc_ht=instagram.famm13-1.fna.fbcdn.net&_nc_cat=109&_nc_oc=Q6cZ2QGn2lASQwQo-EY1RuBQGXd4B7bOwbaLuGa0tfz1CpfOnOwD__nQ5CE5t1BPXpOPQBBKn16wtdFN3saqLkqZ5QyH&_nc_ohc=BIS2sHCKJnQQ7kNvwGPCjXd&_nc_gid=GLJIpNVdsYN8NQcHtMY_PQ&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_Afu1-NalGo9vnpK34IgXnmhq92qEwIzzJCJQwsavLWKMFg&oe=698EC99D&_nc_sid=7a9f4b',
       },
       {
         name: t('store.categories.molded'),
-        img: 'https://scontent.famm7-1.fna.fbcdn.net/v/t39.30808-6/463869455_122114133488552292_3676113582017752048_n.jpg',
+        img: 'https://instagram.famm7-1.fna.fbcdn.net/v/t51.2885-19/464738703_1537686983787349_3949811633865506413_n.jpg?efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4xMDgwLmMyIn0&_nc_ht=instagram.famm7-1.fna.fbcdn.net&_nc_cat=101&_nc_oc=Q6cZ2QEXyfz_ry05nw4ag2zKsGIaOFwv2-YIu7au5SMgDELZC1Uq0-4l0Ro6pkRZ7KWHPjP-BvQl1oIcyIopRgcOmucm&_nc_ohc=Wj_2j2Bhev0Q7kNvwGDR82G&_nc_gid=QGm3E8z6ABoqkyHEnOc__w&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_AfuSRm537TFSUe_QWl5FCfbvJERMU3UicG57D-LUiHkcuA&oe=698EC954&_nc_sid=7a9f4b',
       },
     ],
     [t]
@@ -161,8 +161,8 @@ export const Home = () => {
 
           {/* images unchanged */}
           <div className="grid grid-cols-2 gap-4">
-            <img className="rounded-2xl h-64 object-cover" src="https://scontent.famm10-1.fna.fbcdn.net/v/t39.30808-6/611827048_1306368678194187_6872564038331745294_n.jpg" />
-            <img className="rounded-2xl h-64 object-cover" src="https://scontent.famm11-1.fna.fbcdn.net/v/t39.30808-6/626432085_122201078354552292_7736825017065677994_n.jpg" />
+            <img className="rounded-2xl h-64 object-cover" src="https://scontent.famm6-1.fna.fbcdn.net/v/t39.30808-6/616013146_1316543953843326_5408818414923851489_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=xjkNHdJjCXMQ7kNvwFkD_6R&_nc_oc=AdlLcmtRkq3fqo3H38kIFkpsgNjEgvbFaeda7KaGDMn_AryFfxOsj8RCaLmxKJx0Gp5MwsIY-u7lH6FXNgHhvfdM&_nc_zt=23&_nc_ht=scontent.famm6-1.fna&_nc_gid=5B7jwB1agNqaC2GbWWFJ-w&oh=00_AfsORpuxk6f3JxA9fmYfoTkbbhQXVbS5addcIfgxO338aA&oe=698ECD28" />
+            <img className="rounded-2xl h-64 object-cover" src="https://scontent.famm11-1.fna.fbcdn.net/v/t39.30808-6/626432085_122201078354552292_7736825017065677994_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=3108ot4-n_8Q7kNvwGHo-Oa&_nc_oc=AdmjQDfyrGI-ZcvU6ip1LDQEV3QBK3MECQE6nVyUnaapDejeZO8suOpWbb1DazvGPVALjFTM-YWlYc9Pv_VONFEl&_nc_zt=23&_nc_ht=scontent.famm11-1.fna&_nc_gid=DZ_4hnJ4pnoCqPkO8h1P5A&oh=00_AftNr25ggY3BxO8ryfSUKwxGaeL8eotPsJn_wziJ2FpTDQ&oe=698EBFA3" />
           </div>
         </div>
       </section>
