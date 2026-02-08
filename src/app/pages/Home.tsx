dd43af3a28c21bba30b4109cafe8d924c0ac40ed
baraasowwan1 <105966196+baraasowwan1@users.noreply.github.com>
2026-02-09 00:14:30 +0300

    Update Home.tsx

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -29,11 +29,11 @@ export const Home = () => {
     () => [
       {
         name: t('store.categories.cakes'),
-        img: 'https://instagram.famm13-1.fna.fbcdn.net/v/t51.2885-19/82444459_215139809630697_790629369532907520_n.jpg?efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4xMDgwLmMyIn0&_nc_ht=instagram.famm13-1.fna.fbcdn.net&_nc_cat=109&_nc_oc=Q6cZ2QGn2lASQwQo-EY1RuBQGXd4B7bOwbaLuGa0tfz1CpfOnOwD__nQ5CE5t1BPXpOPQBBKn16wtdFN3saqLkqZ5QyH&_nc_ohc=BIS2sHCKJnQQ7kNvwGPCjXd&_nc_gid=GLJIpNVdsYN8NQcHtMY_PQ&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_Afu1-NalGo9vnpK34IgXnmhq92qEwIzzJCJQwsavLWKMFg&oe=698EC99D&_nc_sid=7a9f4b',
+        img: 'https://scontent.famm10-1.fna.fbcdn.net/v/t39.30808-6/352382050_622778522920898_4607408828112141834_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=rS4rdP-sQJwQ7kNvwEGDgQM&_nc_oc=AdnihdCiiLNIqJHgLk7W9WMee4dbjpJUNHTcDsrZnG-nDbzMyjcq9w0mMlhOXN8hMPxDy1Fc_gPtPEQXZ0UQbTvh&_nc_zt=23&_nc_ht=scontent.famm10-1.fna&_nc_gid=I06-F-2tdJL83XtwtO-OnQ&oh=00_AfuiVgM1rMoEo3l-GJLjIHEuwlsPWtssdxmq7aMANHjouw&oe=698EB975',
       },
       {
         name: t('store.categories.molded'),
-        img: 'https://instagram.famm7-1.fna.fbcdn.net/v/t51.2885-19/464738703_1537686983787349_3949811633865506413_n.jpg?efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4xMDgwLmMyIn0&_nc_ht=instagram.famm7-1.fna.fbcdn.net&_nc_cat=101&_nc_oc=Q6cZ2QEXyfz_ry05nw4ag2zKsGIaOFwv2-YIu7au5SMgDELZC1Uq0-4l0Ro6pkRZ7KWHPjP-BvQl1oIcyIopRgcOmucm&_nc_ohc=Wj_2j2Bhev0Q7kNvwGDR82G&_nc_gid=QGm3E8z6ABoqkyHEnOc__w&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_AfuSRm537TFSUe_QWl5FCfbvJERMU3UicG57D-LUiHkcuA&oe=698EC954&_nc_sid=7a9f4b',
+        img: 'https://scontent.famm7-1.fna.fbcdn.net/v/t39.30808-6/463869455_122114133488552292_3676113582017752048_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=ZCi-mWswkw4Q7kNvwE-slDf&_nc_oc=AdmU18T1G5DlV93FRLm6-_hOuT8Um-IbJWwE8D1_iFHj3Y4-j6WcXxidnuD6Xwck4qAJnGHBrwTwJhFE5RY35s7v&_nc_zt=23&_nc_ht=scontent.famm7-1.fna&_nc_gid=rAty14Ixjgsm7ptGMWnfNg&oh=00_AfsNRfFuuQrI65utb-B_ElRl6N2km0wI0C9UGvNdT1F4DQ&oe=698EBC8F',
       },
     ],
     [t]
@@ -45,7 +45,7 @@ export const Home = () => {
       <section className="relative h-[80vh] flex items-center overflow-hidden">
         <div className="absolute inset-0">
           <ImageWithFallback
-            src="https://lh3.googleusercontent.com/p/AF1QipNC5zVlCNVkZS5zPUsZ2XYNHqxoCSCrZ8UVOt6T=s1360-w1360-h1020-rw"
+            src="https://scontent.famm10-1.fna.fbcdn.net/v/t39.30808-6/352382050_622778522920898_4607408828112141834_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=rS4rdP-sQJwQ7kNvwEGDgQM&_nc_oc=AdnihdCiiLNIqJHgLk7W9WMee4dbjpJUNHTcDsrZnG-nDbzMyjcq9w0mMlhOXN8hMPxDy1Fc_gPtPEQXZ0UQbTvh&_nc_zt=23&_nc_ht=scontent.famm10-1.fna&_nc_gid=I06-F-2tdJL83XtwtO-OnQ&oh=00_AfuiVgM1rMoEo3l-GJLjIHEuwlsPWtssdxmq7aMANHjouw&oe=698EB975"
             className="w-full h-full object-cover"
           />
           <div className="absolute inset-0 bg-accent/40" />
@@ -150,7 +150,11 @@ export const Home = () => {
                   className="flex items-center justify-between p-4 border border-stone-600 rounded-lg hover:bg-secondary transition"
                 >
                   <div className="flex items-center gap-4">
-                    <img src={cat.img} className="w-12 h-12 rounded-full object-cover" />
+                    <ImageWithFallback
+                      src={cat.img}
+                      alt={cat.name}
+                      className="w-12 h-12 rounded-full object-cover"
+                    />
                     <span>{cat.name}</span>
                   </div>
                   <ArrowRight className="rtl:rotate-180" />
@@ -159,10 +163,16 @@ export const Home = () => {
             </div>
           </div>
 
-          {/* images unchanged */}
+          {/* Images unchanged */}
           <div className="grid grid-cols-2 gap-4">
-            <img className="rounded-2xl h-64 object-cover" src="https://scontent.famm6-1.fna.fbcdn.net/v/t39.30808-6/616013146_1316543953843326_5408818414923851489_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=xjkNHdJjCXMQ7kNvwFkD_6R&_nc_oc=AdlLcmtRkq3fqo3H38kIFkpsgNjEgvbFaeda7KaGDMn_AryFfxOsj8RCaLmxKJx0Gp5MwsIY-u7lH6FXNgHhvfdM&_nc_zt=23&_nc_ht=scontent.famm6-1.fna&_nc_gid=5B7jwB1agNqaC2GbWWFJ-w&oh=00_AfsORpuxk6f3JxA9fmYfoTkbbhQXVbS5addcIfgxO338aA&oe=698ECD28" />
-            <img className="rounded-2xl h-64 object-cover" src="https://scontent.famm11-1.fna.fbcdn.net/v/t39.30808-6/626432085_122201078354552292_7736825017065677994_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=3108ot4-n_8Q7kNvwGHo-Oa&_nc_oc=AdmjQDfyrGI-ZcvU6ip1LDQEV3QBK3MECQE6nVyUnaapDejeZO8suOpWbb1DazvGPVALjFTM-YWlYc9Pv_VONFEl&_nc_zt=23&_nc_ht=scontent.famm11-1.fna&_nc_gid=DZ_4hnJ4pnoCqPkO8h1P5A&oh=00_AftNr25ggY3BxO8ryfSUKwxGaeL8eotPsJn_wziJ2FpTDQ&oe=698EBFA3" />
+            <ImageWithFallback
+              src="https://scontent.famm6-1.fna.fbcdn.net/v/t39.30808-6/616013146_1316543953843326_5408818414923851489_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=xjkNHdJjCXMQ7kNvwFkD_6R&_nc_oc=AdlLcmtRkq3fqo3H38kIFkpsgNjEgvbFaeda7KaGDMn_AryFfxOsj8RCaLmxKJx0Gp5MwsIY-u7lH6FXNgHhvfdM&_nc_zt=23&_nc_ht=scontent.famm6-1.fna&_nc_gid=5B7jwB1agNqaC2GbWWFJ-w&oh=00_AfsORpuxk6f3JxA9fmYfoTkbbhQXVbS5addcIfgxO338aA&oe=698ECD28"
+              className="rounded-2xl h-64 object-cover"
+            />
+            <ImageWithFallback
+              src="https://scontent.famm11-1.fna.fbcdn.net/v/t39.30808-6/626432085_122201078354552292_7736825017065677994_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=3108ot4-n_8Q7kNvwGHo-Oa&_nc_oc=AdmjQDfyrGI-ZcvU6ip1LDQEV3QBK3MECQE6nVyUnaapDejeZO8suOpWbb1DazvGPVALjFTM-YWlYc9Pv_VONFEl&_nc_zt=23&_nc_ht=scontent.famm11-1.fna&_nc_gid=DZ_4hnJ4pnoCqPkO8h1P5A&oh=00_AftNr25ggY3BxO8ryfSUKwxGaeL8eotPsJn_wziJ2FpTDQ&oe=698EBFA3"
+              className="rounded-2xl h-64 object-cover"
+            />
           </div>
         </div>
       </section>
